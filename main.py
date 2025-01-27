0d4c763f2bba7cd6e11cc5e098df4a2412576962
Kováts Máté <kovats.mate@budapestschool.org>
2025-01-27 22:49:51 +0100

    Add linting workflow

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""
+This is the main module of the application.
+It contains the entry point and setup for the FastAPI app.
+"""
+
 from typing import Union
 
 from fastapi import FastAPI
@@ -7,9 +12,11 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+    """GET endpoint that returns a simple JSON response."""
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+    """GET endpoint that returns the item_id and query parameter."""
     return {"item_id": item_id, "q": q}
