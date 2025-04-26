99c88b84d9ca9b8e4700cee798dcb1855fe76e65
Kováts Máté <kovats.mate@budapestschool.org>
2025-04-26 14:18:09 +0200

    Enhance unit test documentation for Classroom ID reset functionality

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,10 @@
+"""Unit tests for the model classes."""
+
 from models import Classroom
 
 
 def test_classroom_id_reset():
+    """Test if the Classroom ID counter is properly reset."""
     Classroom.restart_id_counter()
     classroom = Classroom(backend_id="id_1", name="Room 1")
     assert classroom.id == 0
